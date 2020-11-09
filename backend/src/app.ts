63beeaca8efe7de322c1acf745a326adb6385e32
Albert Gergo <gergoalbert.gta@gmail.com>
2020-11-09 20:31:14 +0200

    Socket setup for the backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import http from "http";
 import { notFoundHandler, errorHandler } from "./middlewares/middlewares";
 import api from "./api/api";
+import socket from "./socket";
 
 const app = express();
+const server = http.createServer(app);
+socket(server);
 
 app.use(express.json());
 app.use(helmet());
@@ -13,4 +17,4 @@ app.use("/api/v1", api);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-export default app;
+export default server;
