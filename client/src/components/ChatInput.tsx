26b12a2c9656a3c735dcfe4d93fd6f73b25ab544
Albert Gergo <gergoalbert.gta@gmail.com>
2020-11-13 21:03:01 +0200

    Messages event & chat ui

--- client/src/components/ChatInput.tsx
+++ client/src/components/ChatInput.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-const ChatInput = () => {
+interface ChatInputProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const ChatInput: React.FC<ChatInputProps> = ({ value, onChange }) => {
   return (
     <input
       type="text"
       className="w-11/12 mx-auto block p-2 m-3 text-lg rounded"
+      value={value}
+      onChange={onChange}
     />
   );
 };
---
+++ client/src/components/ServerLogo.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+interface ServerLogoProps {
+  src: string;
+}
+
+export const CreateNewServerButton = () => {
+  return (
+    <button className="btn--circular bg-blue-400 flex justify-center items-center">
+      <svg
+        viewBox="0 0 24 24"
+        width="30"
+        height="30"
+        stroke="currentColor"
+        strokeWidth="2"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <line x1="12" y1="5" x2="12" y2="19"></line>
+        <line x1="5" y1="12" x2="19" y2="12"></line>
+      </svg>
+    </button>
+  );
+};
+
+const ServerLogo: React.FC<ServerLogoProps> = ({ src }) => {
+  return (
+    <a href="/chat">
+      <img className="object-cover btn--circular" src={src} alt="Server logo" />
+    </a>
+  );
+};
+export default ServerLogo;
