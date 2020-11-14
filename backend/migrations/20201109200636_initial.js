3ad80b3d7dc189234b41751da2422a53aeaecb67
Albert Gergo <gergoalbert.gta@gmail.com>
2020-11-14 18:22:10 +0200

    Initial database migration

--- backend/migrations/20201109200636_initial.js
+++ backend/migrations/20201109200636_initial.js
@@ -1,3 +1,51 @@
-exports.up = async (knex) => {};
+const tableNames = {
+  users: "users",
+  servers: "servers",
+  rooms: "rooms",
+  messages: "messages",
+};
 
-exports.down = async (knex) => {};
+function references(table, name, tableName) {
+  table
+    .integer(name)
+    .unsigned()
+    .references("id")
+    .inTable(tableName)
+    .onDelete("cascade")
+    .notNullable();
+}
+
+exports.up = async (knex) => {
+  await knex.schema.createTable(tableNames.users, (table) => {
+    table.increments();
+    table.string("username", 100).notNullable();
+    table.string("password", 100);
+    table.string("email").unique();
+    table.string("profile_picture", 2083);
+    table.timestamps(false, true);
+  });
+  await knex.schema.createTable(tableNames.servers, (table) => {
+    table.increments();
+    references(table, "owner_id", tableNames.users);
+    table.string("name", 100);
+    table.string("icon", 2083);
+  });
+  await knex.schema.createTable(tableNames.rooms, (table) => {
+    table.increments();
+    references(table, "server_id", tableNames.servers);
+    table.string("name", 100);
+  });
+  await knex.schema.createTable(tableNames.messages, (table) => {
+    table.increments();
+    references(table, "sender_id", tableNames.users);
+    references(table, "room_id", tableNames.rooms);
+    table.string("body");
+  });
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists(tableNames.messages);
+  await knex.schema.dropTableIfExists(tableNames.rooms);
+  await knex.schema.dropTableIfExists(tableNames.servers);
+  await knex.schema.dropTableIfExists(tableNames.users);
+};
