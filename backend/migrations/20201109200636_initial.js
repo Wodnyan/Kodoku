a80264dba36680d43ace0539ff7263a1ec4ee44c
Albert Gergo <gergoalbert.gta@gmail.com>
2020-11-14 23:25:06 +0200

    Working on a better database model, added jwt to github/callback endpoint response for authentication

--- backend/migrations/20201109200636_initial.js
+++ backend/migrations/20201109200636_initial.js
@@ -3,6 +3,8 @@ const tableNames = {
   servers: "servers",
   rooms: "rooms",
   messages: "messages",
+  members: "members",
+  provider: "provider",
 };
 
 function references(table, name, tableName) {
@@ -16,6 +18,7 @@ function references(table, name, tableName) {
 }
 
 exports.up = async (knex) => {
+  // USERS
   await knex.schema.createTable(tableNames.users, (table) => {
     table.increments();
     table.string("username", 100).notNullable();
@@ -24,26 +27,48 @@ exports.up = async (knex) => {
     table.string("avatar_url", 2083);
     table.timestamps(false, true);
   });
+  // PROVIDER
+  await knex.schema.createTable(tableNames.provider, (table) => {
+    table.increments();
+    table.string("provider_id");
+    table.string("provider");
+    references(table, "user_id", tableNames.users);
+  });
+  // SERVERS
   await knex.schema.createTable(tableNames.servers, (table) => {
     table.increments();
     references(table, "owner_id", tableNames.users);
     table.string("name", 100);
     table.string("icon", 2083);
+    table.timestamps(false, true);
   });
+  // ROOMS
   await knex.schema.createTable(tableNames.rooms, (table) => {
     table.increments();
     references(table, "server_id", tableNames.servers);
     table.string("name", 100);
+    table.timestamps(false, true);
   });
+  // MESSAGES
   await knex.schema.createTable(tableNames.messages, (table) => {
     table.increments();
     references(table, "sender_id", tableNames.users);
     references(table, "room_id", tableNames.rooms);
+    table.timestamps(false, true);
     table.string("body");
+  });
+  // MEMBERS
+  await knex.schema.createTable(tableNames.members, (table) => {
+    table.increments();
+    references(table, "member_id", tableNames.users);
+    references(table, "server_id", tableNames.servers);
+    table.unique(["member_id", "server_id"]);
+    table.timestamps(false, true);
   });
 };
 
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists(tableNames.members);
   await knex.schema.dropTableIfExists(tableNames.messages);
   await knex.schema.dropTableIfExists(tableNames.rooms);
   await knex.schema.dropTableIfExists(tableNames.servers);
