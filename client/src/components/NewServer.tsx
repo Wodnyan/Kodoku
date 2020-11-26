96d92ea1c539dd5bfe7c7793932075b0faf7a4a1
Albert Gergo <gergoalbert.gta@gmail.com>
2020-11-26 19:39:13 +0200

    Visual indicator for the user to show which form they are viewing

--- client/src/components/NewServer.tsx
+++ client/src/components/NewServer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { API_ENDPOINT } from "../constants";
 import UserContext from "../context/UserContext";
@@ -7,13 +7,24 @@ interface NewServerInputs {
   serverName: string;
   serverIcon: string;
 }
+interface JoinServerInput {
+  serverInviteCode: string;
+}
 
 interface NewServerProps {
   closeOverlay: () => void;
 }
 
+enum CurrentFormState {
+  Create = 0,
+  Join = 1,
+}
+
 const NewServer: React.FC<NewServerProps> = ({ closeOverlay }) => {
   const ref = useRef<null | HTMLDivElement>(null);
+  const [currentForm, setCurrentForm] = useState<
+    CurrentFormState.Create | CurrentFormState.Join
+  >(CurrentFormState.Create);
   useEffect(() => {
     const handle = (e: any) => {
       if (ref.current && e.target === ref.current) {
@@ -31,8 +42,63 @@ const NewServer: React.FC<NewServerProps> = ({ closeOverlay }) => {
       ref={ref}
       className="absolute flex flex-col justify-center items-center h-full w-full bg-black bg-opacity-50"
     >
-      <NewServerForm />
+      <div className="cursor-pointer">
+        <span
+          onClick={() => setCurrentForm(CurrentFormState.Create)}
+          className={`text-white text-4xl ${
+            currentForm === CurrentFormState.Create
+              ? "text-opacity-100"
+              : "text-opacity-50"
+          }`}
+        >
+          Create
+        </span>
+        <span className="text-white text-4xl">/</span>
+        <span
+          onClick={() => setCurrentForm(CurrentFormState.Join)}
+          className={`text-white text-4xl ${
+            currentForm === CurrentFormState.Join
+              ? "text-opacity-100"
+              : "text-opacity-50"
+          }`}
+        >
+          Join
+        </span>
+      </div>
+      {currentForm === CurrentFormState.Create ? (
+        <NewServerForm />
+      ) : (
+        <JoinServerForm />
+      )}
     </div>
+  );
+};
+
+export const JoinServerForm = () => {
+  const { register, handleSubmit, watch } = useForm<JoinServerInput>();
+  const user = useContext(UserContext);
+
+  const onSubmit = async (data: NewServerInputs) => {
+    console.log(data);
+  };
+
+  return (
+    <form className="w-1/3" onSubmit={handleSubmit(onSubmit)}>
+      <img src={String(watch("serverUrl"))} alt="" />
+      <div className="w-full">
+        <label htmlFor="inviteCode" className="text-white">
+          Invite Code
+        </label>
+        <input
+          className="input"
+          name="inviteCode"
+          id="inviteCode"
+          type="text"
+          ref={register({ required: true })}
+        />
+      </div>
+      <button className="text-white">Submit</button>
+    </form>
   );
 };
 
