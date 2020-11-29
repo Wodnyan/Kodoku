320cc39eca5ef3e6bcb129eeefb81ed52ab9dbf1
Albert Gergo <gergoalbert.gta@gmail.com>
2020-11-29 23:16:45 +0200

    Removed unnecessary components and logic

--- client/src/views/Chat.tsx
+++ client/src/views/Chat.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { useHistory } from "react-router-dom";
-import { io, Socket } from "socket.io-client";
 import { getAllServers } from "../api/server";
 import Chat from "../components/Chat";
-import ChatInput from "../components/ChatInput";
 import NewServer from "../components/NewServer";
 import ServerLogo, { CreateNewServerButton } from "../components/ServerLogo";
 import Rooms from "../components/Rooms";
@@ -14,28 +12,11 @@ import { Server, User } from "../types";
 import Members from "../components/Members";
 
 const ChatPage = () => {
-  const [socket, setSocket] = useState<null | Socket>(null);
   const [showNewServerOverlay, setShowNewServerOverlay] = useState(false);
-  const [inputValue, setInputValue] = useState("");
   const [user, setUser] = useState<User | null>(null);
   const [servers, setServers] = useState<[] | Server[]>([]);
 
   const history = useHistory();
-
-  useEffect(() => {
-    const socketio = io("/");
-    setSocket(socketio);
-    socketio.on("connect", (socket: Socket) => {
-      console.log("Connected");
-    });
-    socketio.on("message", (message: any) => {
-      console.log(message);
-    });
-    return () => {
-      socket?.disconnect();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   useEffect(() => {
     (async () => {
@@ -59,21 +40,6 @@ const ChatPage = () => {
 
   const addNewServer = (payload: Server) => {
     setServers((prev) => [...prev, payload]);
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const target = e.currentTarget[0] as HTMLInputElement;
-    console.log(target.value);
-    socket?.emit("message", {
-      sender: "Anon",
-      body: target.value,
-    });
-    setInputValue("");
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
   };
 
   return (
@@ -112,40 +78,14 @@ const ChatPage = () => {
               <section className="bg-red-900 w-1/5 h-full overflow-auto">
                 <Rooms rooms={[...Array(50)]} />
               </section>
-              <section className="bg-blue-400 w-8/12 grid grid-rows-2">
-                <div className="row-span-2">
-                  <Chat />
-                </div>
-                <form onSubmit={handleSubmit} className="bg-blue-600">
-                  <ChatInput
-                    value={inputValue}
-                    onChange={(e) => handleInputChange(e)}
-                  />
-                </form>
+              <section className="bg-blue-400 w-8/12 ">
+                <Chat />
               </section>
               <section className="bg-blue-800 w-1/6 h-screen overflow-auto">
                 {/* <Members /> */}
               </section>
             </Route>
-            <Route exact path="/chat/:serverId/:roomId">
-              <section className="bg-red-900 w-1/3 h-full overflow-auto">
-                {/* <Rooms rooms={[...Array(50)]} /> */}2
-              </section>
-              <section className="bg-blue-400 w-full grid grid-rows-2">
-                {/* <div className="row-span-2">
-                  <Chat />
-                </div>
-                <form onSubmit={handleSubmit} className="bg-blue-600">
-                  <ChatInput
-                    value={inputValue}
-                    onChange={(e) => handleInputChange(e)}
-                  />
-                </form> */}
-              </section>
-              <section className="bg-blue-800 w-1/3 h-screen overflow-auto">
-                {/* <Members /> */}
-              </section>
-            </Route>
+            <Route exact path="/chat/:serverId/:roomId"></Route>
           </Switch>
         </div>
       </Router>
