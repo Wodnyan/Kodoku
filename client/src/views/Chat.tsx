7725da2b9f52db8748e265e9496fbdd925b1c504
Albert Gergo <gergoalbert.gta@gmail.com>
2021-03-12 21:24:56 +0200

    feat: avoid rerendering the rooms view

--- client/src/views/Chat.tsx
+++ client/src/views/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Chat from "../components/Chat";
 import Rooms from "../components/room/Rooms";
@@ -6,6 +6,38 @@ import UserContext from "../context/UserContext";
 import Members from "../components/Members";
 import Servers from "../components/Servers";
 import { useAuth } from "../hooks/api/auth";
+
+const DefaultPage = () => {
+  return (
+    <section className="bg-blue-800 w-full h-screen overflow-auto flex justify-center items-center">
+      <h1 className="text-4xl text-blue-900">Welcome to KODOKU</h1>
+    </section>
+  );
+};
+
+const ServerView = () => {
+  return (
+    <>
+      <section className="w-8/12 bg-gray-600"></section>
+      <section className="bg-gray-700 w-1/6 h-screen overflow-auto">
+        <Members />
+      </section>
+    </>
+  );
+};
+
+const RoomView = () => {
+  return (
+    <>
+      <section className="w-8/12 bg-gray-600">
+        <Chat />
+      </section>
+      <section className="bg-gray-700 w-1/6 h-screen overflow-auto">
+        <Members />
+      </section>
+    </>
+  );
+};
 
 const ChatPage = () => {
   const { user } = useAuth("/");
@@ -17,33 +49,22 @@ const ChatPage = () => {
           <section className="hide-scrollbar bg-gray-800 w-16 h-screen overflow-auto">
             <Servers />
           </section>
+          <Route exact path="/chat">
+            <DefaultPage />
+          </Route>
           <Switch>
-            <Route exact path="/chat">
-              <section className="bg-blue-800 w-full h-screen overflow-auto flex justify-center items-center">
-                <h1 className="text-4xl text-blue-900">Welcome to KODOKU</h1>
-              </section>
-            </Route>
-            <Route exact path="/chat/:serverId">
+            <Route path="/chat/:serverId">
               <section className="bg-gray-700 w-1/5 h-screen">
                 <Rooms />
-              </section>
-              <section className="w-8/12 bg-gray-600"></section>
-              <section className="bg-gray-700 w-1/6 h-screen overflow-auto">
-                <Members />
-              </section>
-            </Route>
-            <Route exact path="/chat/:serverId/:roomId">
-              <section className="bg-gray-700 w-1/5 h-screen">
-                <Rooms />
-              </section>
-              <section className="w-8/12 bg-gray-600">
-                <Chat />
-              </section>
-              <section className="bg-gray-700 w-1/6 h-screen overflow-auto">
-                <Members />
               </section>
             </Route>
           </Switch>
+          <Route exact path="/chat/:serverId">
+            <ServerView />
+          </Route>
+          <Route exact path="/chat/:serverId/:roomId">
+            <RoomView />
+          </Route>
         </div>
       </Router>
     </UserContext.Provider>
