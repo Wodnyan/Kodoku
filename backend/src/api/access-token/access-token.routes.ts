a18be07756c8957fbf1bf37630bcd6983dab91c5
Albert Gergo <gergoalbert.gta@gmail.com>
2021-03-20 20:37:24 +0200

    feat: check if refresh token is blacklisted before supplying new access token

--- backend/src/api/access-token/access-token.routes.ts
+++ backend/src/api/access-token/access-token.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import ErrorHandler from "../../lib/error-handler";
 import { createAccessToken, verifyRefreshToken } from "../../lib/jwt";
 import { limiter } from "../../lib/rate-limiter";
+import { RefreshTokenController } from "../../controllers/refresh-token";
 
 const router = Router();
 
@@ -10,6 +11,15 @@ router.use(limiter(100));
 router.get("/refresh", async (req, res, next) => {
   try {
     const refreshToken = req.cookies.refresh_token;
+    if (!refreshToken) {
+      throw new ErrorHandler(400, "Provide a token");
+    }
+    const isBlackListed = await RefreshTokenController.isTokenBlackListed(
+      refreshToken
+    );
+    if (isBlackListed) {
+      throw new ErrorHandler(403, "Token is blacklisted");
+    }
     const payload = (await verifyRefreshToken(refreshToken)) as any;
     const accessToken = await createAccessToken(payload.userId);
     res.json({
