7e46e8885551c9ca97a69dd05046bab3c726c40e
Albert Gergo <gergoalbert.gta@gmail.com>
2021-03-23 20:53:53 +0200

    refactor: created a file for the accordion

--- client/src/components/settings/Settings.tsx
+++ client/src/components/settings/Settings.tsx
@@ -1,12 +1,9 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import useCloseOnClick from "../../hooks/close-on-click";
+import { AccordionItem } from "../accordion/Accordion";
 
 interface SettingsProps {
   closeSettings: () => void;
-}
-
-interface AccordionItemProps {
-  name: string;
 }
 
 export const Settings: React.FC<SettingsProps> = ({ closeSettings }) => {
@@ -86,34 +83,6 @@ const Accordion = () => {
           risus non commodo elementum. Maecenas semper pulvinar bibendum.
         </AccordionItem>
       ))}
-    </div>
-  );
-};
-
-const AccordionItem: React.FC<AccordionItemProps> = ({ name, children }) => {
-  const [open, setOpen] = useState(false);
-  const accordionRef = useRef<HTMLDivElement | null>(null);
-
-  const openAccordion = () => {
-    if (accordionRef.current) {
-      const node = accordionRef.current;
-      const scrollHeight = node.scrollHeight;
-      node.style.height = open ? `0px` : `${scrollHeight}px`;
-      setOpen((prev) => !prev);
-    }
-  };
-
-  return (
-    <div className="bg-white text-black overflow-hidden">
-      <div className="p-4 bg-red-500 cursor-pointer" onClick={openAccordion}>
-        <h1 className="text-black text-2xl">{name}</h1>
-      </div>
-      <div
-        ref={accordionRef}
-        className="transform h-0 transition-all duration-300 ease-in-out"
-      >
-        {children}
-      </div>
     </div>
   );
 };
