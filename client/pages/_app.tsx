e9ce8fe14fbd24f282ee7db7e4a509b2cf31097f
Albert Gergo <gergoalber.gta@gmail.com>
2021-05-06 22:06:39 +0300

    feat: top loading bar

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,18 @@
 import "../styles/globals.css";
+import Router from "next/router";
 import { AuthProvider } from "../context/auth/AuthProvider";
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#3b49df",
+  className: "bar-of-progress",
+  delay: 200,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return (
