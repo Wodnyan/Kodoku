c3e09d09559887d95b266c1b81c32f99932f6657
Albert Gergo <gergoalbert.gta@gmail.com>
2021-05-11 20:48:48 +0300

    feat: animated room options popout

--- client/modules/RoomListPanel/RoomListPanel.tsx
+++ client/modules/RoomListPanel/RoomListPanel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { useGetAllRoomsOfServer } from "../../hooks/http/rooms";
 import { Room } from "../../types";
@@ -7,18 +7,63 @@ import { Avatar } from "../../components/Avatar/Avatar";
 import { useAuth } from "../../context/auth/AuthProvider";
 import { useRecoilState } from "recoil";
 import { currentServerState } from "../../global-state/current-server";
+import { Button } from "../../components/Button/Button";
+import { CSSTransition } from "react-transition-group";
+import { useClickOutside } from "../../hooks/useClickOutside";
 
 export const RoomList = () => {
   const {
     query: { serverId },
   } = useRouter();
+  const { user } = useAuth();
   const [rooms] = useGetAllRoomsOfServer(Number(serverId));
   const [currentServer] = useRecoilState(currentServerState);
-  const { user } = useAuth();
+
+  const [popout, setPopout] = useState(true);
+
+  const { ref } = useClickOutside(() => setPopout(false));
+
+  function togglePopout() {
+    setPopout((prev) => !prev);
+  }
 
   return (
     <div className={styles.container}>
-      <section>{currentServer.server?.name}</section>
+      <section className={styles.roomOptions}>
+        <div>{currentServer.server?.name}</div>
+        <CSSTransition
+          in={popout}
+          timeout={100}
+          classNames={{
+            enter: styles.popoutBtnEnter,
+            enterDone: styles.popoutBtnEnterActive,
+            exit: styles.popoutBtnExit,
+            exitDone: styles.popoutBtnExitDone,
+          }}
+        >
+          <button className={styles.popoutBtn} onClick={togglePopout}>
+            open
+          </button>
+        </CSSTransition>
+        <CSSTransition
+          unmountOnExit
+          in={popout}
+          timeout={200}
+          classNames={{
+            enter: styles.popoutEnter,
+            enterActive: styles.popoutEnterActive,
+            exit: styles.popoutExit,
+            exitActive: styles.popoutExitActive,
+          }}
+        >
+          <div ref={ref} className={styles.popout}>
+            <Button full>Create Room</Button>
+            <Button full>Create Invite</Button>
+            <Button full>Leave Server</Button>
+            <Button full>Server Options</Button>
+          </div>
+        </CSSTransition>
+      </section>
       <section>
         {rooms.length === 0 && <p>No rooms</p>}
         <ul className={styles.roomList}>
@@ -27,7 +72,6 @@ export const RoomList = () => {
           ))}
         </ul>
       </section>
-      {/* Make this a component */}
       <section className={styles.userSection}>
         <div className={styles.userInfo}>
           <Avatar src={user?.avatar}>{user?.username[0]}</Avatar>
